07bf5ac4719b5d18b90ca9b67c4ce89e0b3228b9
aymanelyamoun <aymanelyamoun93@gmail.com>
2023-12-13 09:54:05 +0000

    adding messages to seeding

--- backend_service/backend/prisma/seed.ts
+++ backend_service/backend/prisma/seed.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 import { CONVERSATION_TYPE } from "@prisma/client";
+import { connect } from 'http2';
 
 const data = Array.from({length:10}).map(()=>{
     return {
@@ -68,34 +69,39 @@ async function main() {
         }
     }
 
-    // add messages to each conversation between users
-    // for (const user of allusers){
-    //     const friends = allusers.filter((u)=>u.id!==user.id);
-    //     for (const friend of friends){
-    //         const conversation = await prisma.conversation.findFirst({
-    //             where:{
-    //                 users:{
-    //                     every:{
-    //                         id:{
-    //                             in:[user.id, friend.id]
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         })
-    //         await prisma.message.create({
-    //             data: {
-    //                 conversationId: conversation.id as never,
-    //                 sender: {
-    //                     connect: { id: user.id }
-    //                 },
-    //                 message: faker.lorem.sentence()
-    //             }
-    //         });
-    //     }
-    // }
+    // add messages to conversations between the first 2 users
+    
+    for (const user of allusers){
+        const friends = allusers.filter((u)=>u.id!==user.id);
+        for (const friend of friends){
 
+            // check if conversation already exists
+            const conversation = await prisma.conversation.findFirst({
+                where:{
+                    users:{
+                        every:{
+                            id:{
+                                in:[user.id, friend.id]
+                            }
+                        }
+                    }
+                }
+            })
 
+            if (!conversation) continue;
+
+            for (let i=0;i<5;i++){
+                await prisma.message.create({
+                    data:{
+                        message:faker.lorem.sentences(2),
+                        conversation: {connect: {id: conversation.id}},
+                        sender:{connect:{id:user.id}},
+                    }
+                })
+            }
+
+        }
+    }
 }
 
 main()

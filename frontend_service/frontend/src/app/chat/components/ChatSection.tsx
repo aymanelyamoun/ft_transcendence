562bcec5e50a212e9436a75c31dc65f062b9759f
ANASPINOZA <anas.adnan2016@gmail.com>
2023-12-16 17:43:53 +0100

    contune in another sesion

--- frontend_service/frontend/src/app/chat/components/ChatSection.tsx
+++ frontend_service/frontend/src/app/chat/components/ChatSection.tsx
@@ -7,6 +7,7 @@ import {
 } from "./ConversationInfo";
 import { MessageProps } from "../../../../../../backend_service/backend/types/chatTypes";
 import avatar from "../../../../public/garou-kid.jpeg";
+import jake from "../../../../public/jakeWithHeadPhones.jpg";
 import Image from "next/image";
 
 import { userId, isAdmin } from "./ConversationInfo";
@@ -55,30 +56,49 @@ const ChatSection = () => {
 export const ConversationChatSection = () => {
   const messagesData = useContext(MessagesContext);
   const [messages, setMessages] = useState<MessageProps[]>([]);
+  const conversation = useContext(LstConversationStateContext);
 
   useEffect(() => {
     setMessages(messagesData);
   }, [messagesData]);
 
   return (
-    <div className="message flex flex-col overflow-y-auto overflow-x-hidden pr-12">
-      {messages.map((message) => {
-        if (message.userId === userId) {
-          return <Message message={message} />;
-        }
-      })}
+    <div className="chatSection flex-grow flex flex-col justify-between">
+      <div className="message flex flex-col overflow-y-auto overflow-x-hidden pr-12">
+        {messages.map((message) => {
+          if (message.userId === userId) {
+            return <Message message={message} />;
+          }
+        })}
+      </div>
+        <TypeMsg
+          userId={userId}
+          conversationId={conversation?.id}
+          // sendMessage={handleSendMessage}
+          messages={messages}
+          setMessages={setMessages}
+        />
     </div>
   );
 };
 
 const Message = ({ message }: { message: MessageProps }) => {
   return (
-    <div className="rcvMsg">
-      <div className=""> 
-        <Image className="rounded-full float-left mr-[10px] mt-[4px] border-[1.5px] border-[#202345]" src={avatar} alt="profile pic" width={42} height={42} />
-        <p className=""> {message.message} </p>
+    <>
+      <div className="sendMsg">
+        <div className=""> 
+          <Image className="rounded-full float-left mr-[10px] mt-[4px] border-[1.5px] border-[#202345]" src={avatar} alt="profile pic" width={43} height={43} />
+          <p className="ml-2 mt-3 "> {message.message} </p>
+        </div>
       </div>
-    </div>
+
+      <div className="rcvMsg">
+        <div className=""> 
+          <Image className="rounded-full float-left mr-[10px] mt-[4px] border-[1.5px] border-[#202345]" src={jake} alt="profile pic" width={43} height={43} />
+          <p className="ml-2 mt-3 "> {message.message} </p>
+        </div>
+      </div>
+    </>
   );
 };
 

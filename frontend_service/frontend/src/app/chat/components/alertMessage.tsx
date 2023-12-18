3f47cfa9666ede3f45f1b5a3e47c1e8df75520aa
ANASPINOZA <anas.adnan2016@gmail.com>
2023-12-18 17:13:53 +0100

    starting on alertMessage component

---
+++ frontend_service/frontend/src/app/chat/components/alertMessage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const alertMessage = ({message , type} : {message:string, type:string}) => {
+
+  return (
+    <div className=" createChannelOverlay flex justify-center items-center ">
+         <div id="AddchannelContainer" className="addChannelModal felx justify-between rounded-[10px] ">
+          {type === "error" ? <p> hello</p>: <p>by</p> }
+        </div>
+    </div>
+  )
+}

9dca7bf6f490051c3d139ee7e0f562a9676b22eb
Oussama Bouadel <obouadel@e2r13p2.1337.ma>
2023-12-19 16:50:58 +0100

    save

---
+++ frontend_service/frontend/src/app/game/singleplayer/page.tsx
@@ -0,0 +1,10 @@
+import Singleplayer from "./Singleplayer";
+import "./globals.css";
+export default function SingleMatch(){
+
+    return (
+        <div>
+            <Singleplayer />
+        </div>
+    )
+}

4d5f564a7ca4e4c7f108d1efda6cdb5d4079fc28
ANASPINOZA <anas.adnan2016@gmail.com>
2023-12-21 14:53:09 +0100

    finished options dropdown handle onclicks

--- frontend_service/frontend/src/app/chat/components/conversationInfo.tsx
+++ frontend_service/frontend/src/app/chat/components/conversationInfo.tsx
@@ -23,7 +23,7 @@ import { SlOptions } from "react-icons/sl";
 
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
-
+import { MdArrowForwardIos } from "react-icons/md";
 
 export const userId = "2e7c4b42-efe4-4b5a-99eb-78d83e26c801";
 export const isAdmin = false;
@@ -258,9 +258,10 @@ const MemberIthem = ({
                   >
                     {option.label}
                     {option.subOptions && (
-                      <Menu as="div" className="relative">
-                        <Menu.Button className="pl-4 text-white">
-                          <span className="ml-2"> &gt; </span>
+                      <Menu as="div" className="relative flex items-center">
+                        <Menu.Button className="pl-4 text-white ">
+                        <MdArrowForwardIos />
+                          {/* <span className="ml-2"> &gt; </span> */}
                         </Menu.Button>
                         <Transition
                           as={Fragment}
@@ -271,7 +272,7 @@ const MemberIthem = ({
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
-                          <Menu.Items className="origin-top-right absolute right-0 mr-[112px] mt-2 w-32 rounded-md shadow-lg bg-[#202446] ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+                          <Menu.Items className="origin-top-right absolute right-0 mr-[112px] mt-[140px] w-32 rounded-md shadow-lg bg-[#202446] ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
                             {option.subOptions.map((subOption, index) => (
                               <Menu.Item key={index}>
                                 {({ active }) => (
@@ -331,11 +332,13 @@ const MemberList = ({}: {}) => {
         })
         .then((data) => {
           setMembers(data);
+          // setMembers((prev) => { return [...prev, data] })
           if (data) setIsSet(true);
         });
     };
     if (conversation?.id !== undefined) fetchFun();
   }, [conversation, isSet]);
+
 
   // console.log("isSet:", isSet);
   // console.log("members:", members);

8762f17fe918a07a522f12724d800fe7960adc9c
itsOussamox <oussaroublard@gmail.com>
2024-01-28 15:18:56 +0100

    :gear: Handled some Build warning

--- frontend_service/frontend/src/app/(auth)/game/components/PlayExtra.tsx
+++ frontend_service/frontend/src/app/(auth)/game/components/PlayExtra.tsx
@@ -24,7 +24,7 @@ export default function PlayExtra({setGameState, gameState}: GameMenuProps) {
             </button>
             <button className='relative sm:text-lg md:text-xl lg:text-2xl xl:text-3x blue-card text-white bg-[#383e6e] hover:bg-[#262b4c] flex-1 w-full'
             onClick={playComputer}>
-                <div className="left-[16%] top-[30%] h-[15%] w-[46%] sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-start absolute font-[poppins]">PRACTIVE WITH BIMO</div>
+                <div className="left-[16%] top-[30%] h-[15%] w-[46%] sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-start absolute font-[poppins]">PRACTICE WITH BIMO</div>
                 <Image alt="img" className="absolute left-[68%] top-[15%] h-[95%] w-[37%] rounded-xl" src='/CyanCard.svg'/>
             </button>
             <button 
